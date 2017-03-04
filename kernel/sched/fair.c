b9c0217427a97fbd9acd1d68784105a3c8ac683e
alireza <sarsanaee@gmail.com>
2017-03-04 22:43:44 +0330

    recording on put_prev_task

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5835,6 +5835,8 @@ static void put_prev_task_fair(struct rq *rq, struct task_struct *prev)
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
 		put_prev_entity(cfs_rq, se);
+		// aghax
+		se->rank = rq->rq_rank++;
 	}
 }
 
